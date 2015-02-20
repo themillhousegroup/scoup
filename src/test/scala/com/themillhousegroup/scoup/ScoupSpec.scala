3086b42fa20b3fd8ef95226a7766d3b40ca6274c
John Marshall <john.marshall2@auspost.com.au>
2015-02-20 15:24:50 +1100

    Fix race condition in tests causing build failures

--- src/test/scala/com/themillhousegroup/scoup/ScoupSpec.scala
+++ src/test/scala/com/themillhousegroup/scoup/ScoupSpec.scala
@@ -5,6 +5,8 @@ import org.specs2.mock.Mockito
 import org.jsoup.Connection
 import org.specs2.specification.Scope
 import com.themillhousegroup.scoup.traits.{ UserAgents, Waits, UserAgent }
+import scala.concurrent.{ Await, Future }
+import org.jsoup.nodes.Document
 
 class ScoupSpec extends Specification with Mockito {
 
@@ -16,6 +18,10 @@ class ScoupSpec extends Specification with Mockito {
     val mockJsoup = mock[JSoupProvider]
     mockJsoup.connect(anyString) returns mockConnection
     val testScoup = new Scoup(mockJsoup)
+
+    def waitFor(f: => Future[Document]) = {
+      Await.result(f, Waits.fifteenSecondDuration)
+    }
   }
 
   class ScoupImplicitScope extends MockScoupScope with ScoupImplicits {}
@@ -23,13 +29,13 @@ class ScoupSpec extends Specification with Mockito {
   "Scoup User-Agent support" should {
 
     "Connect with a reasonable User Agent by default" in new ScoupImplicitScope {
-      testScoup.parse("foo")
+      waitFor(testScoup.parse("foo"))
 
       there was one(mockConnection).userAgent(UserAgents.macChromeUserAgent)
     }
 
     "Accept an overridden User Agent" in new MockScoupScope {
-      testScoup.parse("foo")(Waits.fifteenSecondDuration, UserAgent("blah"))
+      waitFor(testScoup.parse("foo")(Waits.fifteenSecondDuration, UserAgent("blah")))
 
       there was one(mockConnection).userAgent("blah")
     }
@@ -38,13 +44,13 @@ class ScoupSpec extends Specification with Mockito {
   "Scoup Timeout support" should {
 
     "Connect with a reasonable timeout by default" in new ScoupImplicitScope {
-      testScoup.parse("foo")
+      waitFor(testScoup.parse("foo"))
 
       there was one(mockConnection).timeout(Waits.fifteenSecondsInMillis)
     }
 
     "Accept an overridden User Agent" in new MockScoupScope {
-      testScoup.parse("foo")(Waits.sixtySecondDuration, UserAgent("blah"))
+      waitFor(testScoup.parse("foo")(Waits.sixtySecondDuration, UserAgent("blah")))
 
       there was one(mockConnection).timeout(Waits.sixtySecondsInMillis)
     }
@@ -53,13 +59,13 @@ class ScoupSpec extends Specification with Mockito {
   "Scoup GET and POST support" should {
 
     "Allow a GET request to be made" in new ScoupImplicitScope {
-      testScoup.parse("foo")
+      waitFor(testScoup.parse("foo"))
 
       there was one(mockConnection).get
     }
 
     "Allow a POST request to be made" in new ScoupImplicitScope {
-      testScoup.parsePost("foo", Map())
+      waitFor(testScoup.parsePost("foo", Map[String, String]()))
 
       there was one(mockConnection).post
     }
