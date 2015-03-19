eda3633e20cd3629930b9704d4bc169b7a49ca2d
John Marshall <john@themillhousegroup.com>
2015-03-19 16:56:17 +1100

    Tests for attribute regex finding

---
+++ src/test/scala/com/themillhousegroup/scoup/AttributeRegexSpec.scala
@@ -0,0 +1,58 @@
+package com.themillhousegroup.scoup
+
+import org.specs2.mutable.Specification
+import com.themillhousegroup.scoup.test.HtmlFixtures
+import org.jsoup.nodes.Element
+
+class AttributeRegexSpec extends Specification with ScoupImplicits with HtmlFixtures {
+
+  val doc = Scoup.parseHTML(variousAttributes)
+
+  "AttributeRegex" should {
+    "Be able to defined attributes as a Some" in {
+      val item = doc.select("h1").head
+      item must not beNull
+
+      item.attributeRegex("id".r) must beSome[String]
+      item.attributeRegex("id".r).get must beEqualTo("top")
+    }
+
+    "Consider a missing attribute as a None" in {
+      val item = doc.select("h2#middle").head
+      item must not beNull
+
+      item.attributeRegex("class".r) must beNone
+    }
+
+    "Consider an empty attribute as a None" in {
+      val item = doc.select("h2#bottom").head
+      item must not beNull
+
+      item.attributeRegex("class".r) must beNone
+    }
+  }
+
+  "ElementsAttributeRegex" should {
+    "Be able to find the first defined attribute as a Some" in {
+      val items = doc.select("h2")
+      items must not beNull
+
+      items.attribute("id") must beSome[String]
+      items.attribute("id").get must beEqualTo("middle")
+    }
+
+    "Consider a missing attribute as a None" in {
+      val items = doc.select("h2")
+      items must not beNull
+
+      items.attribute("class") must beNone
+    }
+
+    "Consider an empty attribute as a None" in {
+      val items = doc.select("h2")
+      items must not beNull
+
+      items.attribute("class") must beNone
+    }
+  }
+}
