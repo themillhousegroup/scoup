74edaca0c971a7dbdd1f8c33cf893f3cdf347095
John Marshall <john@themillhousegroup.com>
2015-03-19 16:57:13 +1100

    Move regex into helper object

--- src/main/scala/com/themillhousegroup/scoup/traits/AttributeOption.scala
+++ src/main/scala/com/themillhousegroup/scoup/traits/AttributeOption.scala
@@ -1,8 +1,10 @@
 package com.themillhousegroup.scoup.traits
 
 import org.jsoup.nodes.Element
+import org.jsoup.nodes.Attributes
 import org.jsoup.select.Elements
 import scala.util.matching.Regex
+import scala.collection.JavaConverters._
 
 /** Allow the attributes of an Element to be treated like a Scala Option */
 trait AttributeOption {
@@ -10,9 +12,8 @@ trait AttributeOption {
 
   def attribute(name: String): Option[String] = EmptyStringToOption(target.attr(name))
 
-  def attributeRegex(nameRegex: Regex): Option[String] = {
-    None
-  }
+  def attributeRegex(nameRegex: Regex): Option[String] = AttributeRegexToOption(target.attributes, nameRegex)
+
 }
 
 trait ElementsAttributeOption {
@@ -25,5 +26,12 @@ object EmptyStringToOption {
   def apply(ss: String): Option[String] = ss match {
     case "" => None
     case s: String => Some(s)
+  }
+}
+
+object AttributeRegexToOption {
+  def apply(attributes: Attributes, nameRegex: Regex): Option[String] = {
+    val atts = attributes.asList.asScala
+    atts.find(att => nameRegex.findFirstIn(att.getKey).isDefined).flatMap(att => EmptyStringToOption(att.getValue))
   }
 }
