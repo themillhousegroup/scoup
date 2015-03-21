a8e5e427c15b76de041f5bf475ad473a2b05d6dc
John Marshall <john@themillhousegroup.com>
2015-03-21 15:23:36 +1100

    Document positioning (aka isBefore) working but needs cleaning up

--- src/main/scala/com/themillhousegroup/scoup/traits/DocumentPositioning.scala
+++ src/main/scala/com/themillhousegroup/scoup/traits/DocumentPositioning.scala
@@ -1,10 +1,76 @@
 package com.themillhousegroup.scoup.traits
 
 import org.jsoup.nodes.Node
+import scala.annotation.tailrec
 
 trait DocumentPositioning extends ElementTarget {
 
-  def isBefore(other: Node): Boolean = {
-    true
+  def isBefore(other: Node): Boolean =
+    isBefore(Option(target), Option(other))
+
+  /**
+   * Logic:
+   * - If other is my peer, compare indices (negative means I'm in front)
+   * - If other is my child, then I must be before it
+   * - If I am the child of other, then I must be after it
+   *
+   * - Otherwise, perform "isBefore" on our parents recursively
+   *
+   * Args are Options to make null-safe operation easier to read
+   */
+  @tailrec
+  private def isBefore(me: Option[Node], other: Option[Node]): Boolean = {
+
+    if (other.isEmpty) {
+      false
+    } else {
+      if (arePeers(me, other)) {
+        (comparePeerIndices(me, other) < 0)
+      } else {
+        if (isAChildOf(me, other)) {
+          true
+        } else if (isAChildOf(other, me)) {
+          false
+        } else {
+          //          println(s"REC Comparing ${parentOf(me)} and ${parentOf(other)}")
+          isBefore(parentOf(me), parentOf(other))
+        }
+      }
+    }
+  }
+
+  private def isAChildOf(me: Option[Node], other: Option[Node]): Boolean = {
+    val r = for {
+      m <- me
+      o <- other
+      c = (m.childNodes != null) && m.childNodes.contains(o)
+    } yield c
+
+    r.getOrElse(false)
+  }
+
+  private def arePeers(me: Option[Node], other: Option[Node]) = {
+    val r = for {
+      m <- me
+      o <- other
+      p = (m.parent == o.parent)
+    } yield p
+    r.getOrElse(false)
+  }
+
+  private def comparePeerIndices(me: Option[Node], other: Option[Node]): Int = {
+    val r = for {
+      m <- me
+      o <- other
+      i = m.siblingIndex - o.siblingIndex
+    } yield i
+    r.getOrElse(0)
+  }
+
+  /** Find my parent. If I don't have one, return self */
+  private def parentOf(n: Option[Node]): Option[Node] = {
+    n.map { node =>
+      Option(node.parent).getOrElse(node)
+    }
   }
 }
--- src/test/scala/com/themillhousegroup/scoup/DocumentPositioningSpec.scala
+++ src/test/scala/com/themillhousegroup/scoup/DocumentPositioningSpec.scala
@@ -7,20 +7,73 @@ import org.jsoup.nodes.Element
 class DocumentPositioningSpec extends Specification with ScoupImplicits with HtmlFixtures {
 
   val doc = Scoup.parseHTML(nestedList)
+  val top = doc.select("#wrapper").head
 
-  "document positioning" should {
+  val firstList = doc.select("#firstList").head
+  val secondList = doc.select("#secondList").head
+
+  val item1 = doc.select("li#l2i1").head
+  val item2 = doc.select("li#l2i2").head
+  val item3 = doc.select("li#l2i3").head
+  val l3item3 = doc.select("li#l3i3").head
+
+  "document positioning (basics)" should {
+
+    "Never consider null as being before self" in {
+      secondList.isBefore(null) must beFalse
+    }
+
+    "Never consider self as being before self" in {
+      secondList.isBefore(secondList) must beFalse
+      item3.isBefore(item3) must beFalse
+    }
+  }
+
+  "document positioning (children)" should {
     "Be able to determine if a node is before another node" in {
-      val item2 = doc.select("li#l2i2").head
-      item2 must not beNull
+      secondList.isBefore(item3) must beTrue
+    }
+  }
 
-      val item1 = doc.select("li#l2i1").head
-      item1 must not beNull
-
-      val item3 = doc.select("li#l2i3").head
-      item3 must not beNull
-
-      item2.isBefore(item1) must beFalse
+  "document positioning (peers)" should {
+    "Be able to determine if a node is before another node" in {
       item2.isBefore(item3) must beTrue
+    }
+
+    "Be able to determine if a node is not before another node" in {
+      item2.isBefore(item1) must beFalse
+    }
+  }
+
+  "document positioning (same level but in different trees)" should {
+    "Be able to determine if a node is before another node" in {
+      item2.isBefore(l3item3) must beTrue
+    }
+
+    "Be able to determine if a node is not before another node" in {
+      l3item3.isBefore(item1) must beFalse
+    }
+  }
+
+  "document positioning (one degree different)" should {
+    "Be able to determine if a node is before another node" in {
+
+      firstList.isBefore(item3) must beTrue
+    }
+
+    "Be able to determine if a node is not before another node" in {
+      item2.isBefore(firstList) must beFalse
+    }
+  }
+
+  "document positioning (two degrees different)" should {
+    "Be able to determine if a node is before another node" in {
+
+      top.isBefore(item3) must beTrue
+    }
+
+    "Be able to determine if a node is not before another node" in {
+      item2.isBefore(top) must beFalse
     }
   }
 }
