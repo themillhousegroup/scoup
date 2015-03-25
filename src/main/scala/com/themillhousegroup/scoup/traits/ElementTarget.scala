247d4c7f274e39502b512aff2dec5b21c2a223f4
John Marshall <john@themillhousegroup.com>
2015-03-25 14:24:37 +1100

    Tiny refactor to DRY up the *Target trait

--- src/main/scala/com/themillhousegroup/scoup/traits/ElementTarget.scala
+++ src/main/scala/com/themillhousegroup/scoup/traits/ElementTarget.scala
@@ -3,12 +3,14 @@ package com.themillhousegroup.scoup.traits
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
+abstract trait Target[T] {
+  val target: T
+}
+
 /** A trait that expects to be pimped onto a JSoup Element */
-abstract trait ElementTarget {
-  val target: Element
+abstract trait ElementTarget extends Target[Element] {
 }
 
 /** A trait that expects to be pimped onto a JSoup Elements */
-abstract trait ElementsTarget {
-  val target: Elements
+abstract trait ElementsTarget extends Target[Elements] {
 }
