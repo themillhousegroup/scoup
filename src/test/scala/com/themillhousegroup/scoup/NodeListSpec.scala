08bf9f0ad62630c99e6427535ce7de43d0949a1b
John Marshall <john@themillhousegroup.com>
2015-09-25 11:54:31 +1000

    Test for implicit list conversion

---
+++ src/test/scala/com/themillhousegroup/scoup/NodeListSpec.scala
@@ -0,0 +1,28 @@
+package com.themillhousegroup.scoup
+
+import org.specs2.mutable.Specification
+import com.themillhousegroup.scoup.test.HtmlFixtures
+import org.jsoup.nodes._
+
+class NodeListSpec extends Specification with ScoupImplicits with HtmlFixtures {
+
+  val doc = Scoup.parseHTML(nestedList)
+  val fullDoc = Scoup.parseHTML(fullHtml)
+
+  "Node list conversion implicit" should {
+    "Be able to use Scala collection functions on JSoup methods that return Java lists of Node" in {
+      val items = doc.childNodes
+      items must not beNull
+
+      items.find(_.nodeName == "html") must beSome[Node]
+    }
+
+    "Be able to use Scala collection functions on JSoup methods that return Java lists of TextNode" in {
+      val items = doc.select("#l3i2").first.textNodes
+
+      items must not beNull
+
+      items.find(_.asInstanceOf[TextNode].text == "Two") must beSome[Node]
+    }
+  }
+}
