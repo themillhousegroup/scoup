008b6e94a482fd53b61764104a7886ec9b503a0a
John Marshall <john@themillhousegroup.com>
2015-09-25 11:11:18 +1000

    Extra implicit to work with Java Lists of Nodes

--- src/main/scala/com/themillhousegroup/scoup/ScoupImplicits.scala
+++ src/main/scala/com/themillhousegroup/scoup/ScoupImplicits.scala
@@ -1,13 +1,15 @@
 package com.themillhousegroup.scoup
 
 import org.jsoup.select.Elements
-import org.jsoup.nodes.Element
+import org.jsoup.nodes._
 import scala.collection.JavaConverters._
 import com.themillhousegroup.scoup.traits._
 
 trait ScoupImplicits {
   implicit def enrichElements(xs: Elements) = new RichElements(xs)
   implicit def enrichElement(el: Element) = new RichElement(el)
+
+  implicit def enrichNodeList[N <: Node](l: java.util.List[N]) = new RichNodeList(l)
 }
 
 /**
@@ -31,5 +33,11 @@ class RichElement(val target: Element)
     extends ClosestElement
     with DocumentPositioning
     with AttributeOption {
+}
 
+class RichNodeList[N <: Node](val target: java.util.List[N]) {
+
+  def iterator: Iterator[Node] = {
+    target.asScala.iterator
+  }
 }
